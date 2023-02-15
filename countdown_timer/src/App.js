65adfb7e9df8096d22bf8f5be9e7266a3a7c27bc
Shubham <shubhamlahase@hmail.com>
2023-02-15 21:04:08 +0530

    done half - can't get the setInterval stop

--- countdown_timer/src/App.js
+++ countdown_timer/src/App.js
@@ -1,16 +1,45 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 
+const App = () => {
+  const [startingPoint, setStartingPoint] = useState("");
+  const inputValue = startingPoint;
+  const getValue = (event) => {
+    let numericval = Number(event.target.value);
+    // console.log(typeof(numericval));
+    setStartingPoint(numericval);
+  };
 
+  let Int;
 
+  const timer = (event) => {
+    // console.log(event);
+    if (event.key === "Enter") {
+         Int = setInterval(() => {
+          if(startingPoint > 1){
+            setStartingPoint((startingPoint) => startingPoint - 1);
+          }
+        }, 1000);
+    }
+  };
 
-function App() {
-
+  // if(startingPoint === 0) {
+  //   clearInterval(Int);
+  // }
 
   return (
-    <div className="App">
-      react CRA template cleaned and compiled sucessfully
-    </div>
+    <>
+      <input
+        id="timeCount"
+        type="number"
+        placeholder="Enter the Timer value"
+        onChange={getValue}
+        onKeyDown={timer}
+      ></input>
+      <div id="current-time">{startingPoint}</div>
+    </>
   );
-}
+};
 
 export default App;
