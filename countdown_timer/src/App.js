74fc4ff684cb3c4cb1f9bce0dda749dbddff43c6
Shubham <shubhamlahase@hmail.com>
2023-02-15 12:01:25 +0530

    React CRA template cleaned - initial commit

---
+++ countdown_timer/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      react CRA template cleaned and compiled sucessfully
+    </div>
+  );
+}
+
+export default App;
