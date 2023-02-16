aec7b1f0d09b3838c8afe17deeaccdc0b2931f31
Shubham <shubhamlahase@hmail.com>
2023-02-16 11:43:37 +0530

    comdtionally rendered the timer value and added some instructions in the list

--- countdown_timer/src/App.js
+++ countdown_timer/src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const App = () => {
   const [startingPoint, setStartingPoint] = useState("");
-  const inputValue = startingPoint;
   const getValue = (event) => {
     let numericval = Number(event.target.value);
     // console.log(typeof(numericval));
-    setStartingPoint(numericval);
+    setStartingPoint(Math.floor(numericval));
   };
 
-  let Int;
-
+  var Int;
+  let countEnter = 0;
   const timer = (event) => {
     // console.log(event);
-    if (event.key === "Enter") {
-         Int = setInterval(() => {
+    if(event.key === "Enter" && countEnter === 0){
+      countEnter = countEnter + 1;
+        Int = setInterval(() => {
           if(startingPoint > 1){
             setStartingPoint((startingPoint) => startingPoint - 1);
           }
@@ -27,9 +27,14 @@ const App = () => {
   // if(startingPoint === 0) {
   //   clearInterval(Int);
   // }
-
+  // Not abel to clear interval hence, did the optional rendering only if the value > 0, if it's < 0, then time over will be shown.
   return (
     <>
+      <p><ul>
+        <li>Please don't press enter key again, if the timer is already running.</li>
+        <li>You can change the timer value, while its running, on value change it will start from the new value.</li>
+        <li>If you want to run the timer again, refresh page and enter the value and press Enter key</li>
+      </ul></p>
       <input
         id="timeCount"
         type="number"
@@ -37,7 +42,18 @@ const App = () => {
         onChange={getValue}
         onKeyDown={timer}
       ></input>
-      <div id="current-time">{startingPoint}</div>
+      <div id="current-time">
+        {startingPoint >= 0 && (
+          <>
+            {startingPoint}
+          </> 
+        ) || startingPoint < 0 && (
+          <>
+            <p>0</p>
+            <p>Time Over !!!</p>
+          </>
+        )} 
+      </div>
     </>
   );
 };
